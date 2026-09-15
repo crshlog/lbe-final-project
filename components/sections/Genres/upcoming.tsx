9182c13ad164fac26ba857036ba515d6b7583bc1
Hady <hadyrizq9@gmail.com>
2026-09-15 18:14:43 +0700

    feat: add homepage

---
+++ components/sections/Genres/upcoming.tsx
@@ -0,0 +1,10 @@
+export default function Upcoming() {
+  return (
+    <a
+      href="https://www.google.com"
+      className="relative ease-in-out duration-300 hover:scale-105 font-['Iosevka_Charon:Bold',sans-serif] h-32 leading-[normal] left-16 not-italic text-[96px] text-white w-fit    "
+    >
+      {`>> UPCOMING`}
+    </a>
+  );
+}
